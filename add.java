4e9cd12ed2a12fdcaecfe3078171958f22ffa672
jahnavi <gjahnavireddy1@gmail.com>
2026-03-04 11:16:51 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,5 +1,16 @@
-public class add {
+import java.util.Scanner;
+
+public class Odd_Even {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        int n = s.nextInt(); // Read user input
+
+        if (n % 2 == 0) {
+            System.out.println("The given number " + n + " is Even ");
+        } else {
+            System.out.println("The given number " + n + " is Odd ");
+        }
+        s.close(); // Close the scanner
     }
 }
